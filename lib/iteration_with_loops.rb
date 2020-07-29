95053bc71825a06a4ce27fe725bcb160013601fd
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-29 19:39:02 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ results = []
   while array_index < src.length do
     element_index = 0
     while element_index < src[array_index].length do
-      src[array_index].select { |x| x.even? }
+      even_elements = src[array_index].select { |x| x.even? }
       element_index += 1
     end
     array_index += 1
