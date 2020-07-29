d2730c16eab2405fa851350c6d3d9ca6775b2439
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-29 19:31:40 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ results = []
   while array_index < src.length do
     element_index = 0
     while element_index < src[array_index].length do
-      if src[array_index] % 2 = 0
+      if src[array_index] % 2 == 0
         results.push(src[array_index])
       end
       element_index += 1
